c7041065af9868c64522e971a61c8c874d95577f
AsadillahRamadhan <curutcepat@gmail.com>
2025-04-08 00:30:48 +0700

    [UPDATE] update static to dynamic path

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import uuid
 from modules.process import PDFEstatementProcessor
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['OUTPUT_FOLDER'] = 'output'
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, 'uploads')
+app.config['OUTPUT_FOLDER'] = os.path.join(BASE_DIR, 'output')
 
 processor = PDFEstatementProcessor()
 
